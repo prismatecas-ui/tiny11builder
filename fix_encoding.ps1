f29d2533117e864056a7e08af4cd4fed090af7f7
prismatecas-ui <prismatecas@gmail.com>
2026-03-06 15:29:04 -0300

    chore: remove temporary encoding fix script

--- fix_encoding.ps1
+++
@@ -1,3 +0,0 @@
-$path = 'C:\Users\braulio.augusto\Documents\Git\tiny11builder\Tiny11Gui.ps1'
-$content = Get-Content $path -Raw -Encoding UTF8
-Set-Content -Path $path -Value $content -Encoding UTF8
